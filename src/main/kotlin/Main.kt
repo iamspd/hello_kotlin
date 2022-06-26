b1deb72a2098f0738d02f63e4fd526118ecfc021
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2022-06-26 11:54:00 -0400

    initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+/**
+ * codeLab #1: fun printHello() {
+ *                  println("Hello world!")
+ *            }
+ *
+ * ----------- method or function call defined by 'fun' keyword
+ *             function name 'printHello' - parenthesis are there for accepting parameters or arguments
+ *             there is no need to write return type if function does not return anything
+ *             no need to give semicolon at the end of the line, is ok otherwise!
+ */
+
+/** codeLab #2: 2.times(3)
+ *
+ * ------------ kotlin keeps numbers as primitive, but it lets
+ *              you call methods on them as they were objects
+ *
+ */
+
+fun main() {
+    println("Hello world!")
+
+    
+    val i: Int = 8
+
+}
